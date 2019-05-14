d633146724646c6568beb7c565d52571804b4ed7
TITREN Loïc <loic.titren@epitech.eu>
2019-05-14 23:21:22 +0200

    Added return 84 tests in bash tests

--- tests/test_asm.sh
+++ tests/test_asm.sh
@@ -18,27 +18,40 @@ function test {
     make re -C ..
     for file in ./*.s
     do
+        tmp=0
         timeout $$ &
         timeout_pid=$!
         echo
         echo "Testing champion : " $file
         ./../asm/asm $file &> /dev/null
-        cat ${file%.*}.cor > .tmp
+        if [ $? -eq 84 ]; then
+            tmp=$(($tmp+1))
+        fi
+        cat ${file%.*}.cor &> .tmp
         ./corewar_binaries/asm/asm $file &> /dev/null
-        diff .tmp ${file%.*}.cor
-        if [ $? -ne 0 ] ; then
-            if [ ! -f ${file%.*}.cor ]; then
+        if [ $? -eq 84 ]; then
+            if [ $tmp -eq 1 ]; then
+                tmp=$(($tmp+1))
                 echo -ne "\t${GREEN}OK${NC}\n"
                 valid=$(($valid+1))
             fi
-            echo -ne "\t${RED}KO${NC}\n"
-            echo "YOU:"
-            hexdump -C .tmp
-            echo "MOULI:"
-            hexdump -C ${file%.*}.cor
-        else
-            echo -ne "\t${GREEN}OK${NC}\n"
-            valid=$(($valid+1))
+        fi
+        if [ $tmp -ne 2 ]; then
+            diff .tmp ${file%.*}.cor
+            if [ $? -ne 0 ]; then
+                if [ ! -f ${file%.*}.cor ]; then
+                    echo -ne "\t${GREEN}OK${NC}\n"
+                    valid=$(($valid+1))
+                fi
+                echo -ne "\t${RED}KO${NC}\n"
+                # echo "YOU:"
+                # hexdump -C .tmp
+                # echo "MOULI:"
+                # hexdump -C ${file%.*}.cor
+            else
+                echo -ne "\t${GREEN}OK${NC}\n"
+                valid=$(($valid+1))
+            fi
         fi
         total=$(($total+1))
         kill $timeout_pid
@@ -55,7 +68,7 @@ function display {
     else
         echo -ne "${GREEN}"
     fi
-    echo "${result}%"
+    echo "${result}% => $valid / $total"
     echo -ne "${NC}"
 }
 
